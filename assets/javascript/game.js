5c247d19029c0a8c784730cee772bbee434be327
unknown <erichung.94@gmail.com>
2019-03-05 15:40:02 -0600

    completed javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,56 @@
-// Randomly generate a letter, store into var.
-// Compare letter generated with guess (we can adjust this to lower case)
-// Have a set number of guesses to start out the game, and -- for every guess.
-// Display "Guesses so far" (create an array for this), and compare with future guesses so that they don't reduce number of guesses left
-// Create counter for Wins and Losses.
-    var chars = ["a","b","c","d","e","f","g","h","i","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    var wins = 0;
-    var losses = 0;
-    var numberOfGuesses = 9;
-    var playerGuess=[];
-    var computerGuess=[];
-    
-    //generating the random letter
-    var rChar = chars[Math.floor(Math.random() * chars.length)];
-    console.log(rChar);
-    computerGuess.push(rChar);
+//Array of possible computer choices
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    //Grabbing user input
-    document.onkeyup = function(input){
-    var userInput = input.key.toLowerCase();
-    console.log(computerGuess[0]);
-    console.log(numberOfGuesses);
+//Variables for tracking players wins, losses, & guesses left
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var lettersGuessed = [];
+var compGuess = computerChoices[Math.floor(Math.random()* computerChoices.length)];
 
-    //Checking user input is a letter and is not guessed before.
-    if (chars.indexOf(userInput)>=0 && playerGuess.indexOf(userInput)<0){
-        if (userInput !== computerGuess[0] && numberOfGuesses > 0)
-        {
-        playerGuess.push(userInput);
-        numberOfGuesses--;
+//Reset Function
+function reset() {
+	compGuess = computerChoices[Math.floor(Math.random()* computerChoices.length)];
+	guessesLeft = 9;
+	lettersGuessed.length=0;
+}
+
+//Game
+document.onkeyup = function(event) {
+    var playerGuess = event.key;
+
+//Checks to see if the player guess is a letter, and is not a letter previously guessed
+if (computerChoices.indexOf(playerGuess) >= 0 && lettersGuessed.indexOf(playerGuess) < 0) 
+{
+    if ((playerGuess === compGuess) && (guessesLeft >= 1)) {
+        wins++;
+        alert("You win!");
+	    reset();
+    }
+    else if ((playerGuess !== compGuess) && (guessesLeft >= 1)) {
+        guessesLeft--;
+        if (guessesLeft > 0){
+            lettersGuessed.push(playerGuess);
         }
-        //You lose scenario
-        else if (userInput === computerGuess[0] && numberOfGuesses > 0)
-        {
-            alert("You Win!");
-            wins++;
-            numberOfGuesses = 9;
-            playerGuess.length = 0;
-            var rChar = chars[Math.floor(Math.random() * chars.length)];
-            computerGuess.push(rChar)
-            console.log(computerGuess[0]);
-        }
-        else
-        {
-            alert("You Lose!");
+        else if (guessesLeft == 0){
             losses++;
-            numberOfGuesses = 9;
-            playerGuess.length = 0;
-            var rChar = chars[Math.floor(Math.random() * chars.length)];
-            computerGuess.push(rChar)
-            console.log(computerGuess[0]);
+            alert("You lose ):");
+            reset();
         }
-    }    
-       
+    }	
     else {
-        alert("Please input a valid character");
-    }    
+        losses++;
+        alert("You lose ):");
+	    reset();
+    }
+}
+console.log(compGuess);
+var html = 
+           "<h1>The Psychic Game</h1>" +
+           "<p>Guess what letter I'm thinking of!</p>" +
+           "<p>Wins: " + wins + "</p>" +
+           "<p>Losses: " + losses + "</p>" +
+           "<p>Guesses left: " + guessesLeft + "</p>" +
+           "<p>Your guesses so far: " + lettersGuessed + " </p>";
+           document.querySelector("#game").innerHTML = html;
 }
